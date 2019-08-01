052c8f98788f68c62ea2e9594324c2dabec99a48
AaronMillOro <aaron.ibt@gmail.com>
2019-08-01 20:35:52 +0200

    Full text search function, now search query look for a given term in all the fields from the SQL database

--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Mineral
@@ -25,7 +26,18 @@ def mineral_details(request, pk):
 def mineral_search(request):
     """Get all minerals from a query search (term)"""
     term = request.GET.get('query')
-    min_query = Mineral.objects.order_by('name').filter(name__icontains=term)
+    min_query = Mineral.objects.order_by('name').filter(
+    Q(name__icontains=term)|Q(image_caption__icontains=term)|
+    Q(category__icontains=term)|Q(formula__icontains=term)|
+    Q(strunz_classification__icontains=term)|Q(color__icontains=term)|
+    Q(crystal_system__icontains=term)|Q(crystal_symmetry__icontains=term)|
+    Q(cleavage__icontains=term)|Q(category__icontains=term)|
+    Q(mohs_scale_hardness__icontains=term)|Q(luster__icontains=term)|
+    Q(streak__icontains=term)|Q(diaphaneity__icontains=term)|
+    Q(optical_properties__icontains=term)|Q(unit_cell__icontains=term)|
+    Q(refractive_index__icontains=term)|Q(group__icontains=term)|
+    Q(crystal_habit__icontains=term)|Q(specific_gravity__icontains=term)
+    )
     rand_min = Mineral.objects.order_by('?').first()
     # 'minerals' and 'rand_min' are dictionaries sent to template
     return render(request,'mineral_search.html',{
