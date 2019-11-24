6a9a110ae8c2d8340153cf49b5bb54700c252634
AaronMan <aaron.ibt@gmail.com>
2019-11-24 05:11:51 +0100

    ADD: Production settings

--- mineral_catalog/mineral_catalog/settings.py
+++ mineral_catalog/mineral_catalog/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+def get_env_variable(var_name):
+    try:
+        return os.environ[var.name]
+    except KeyError:
+        error_msg = "Set {} env variable".format(var_name)
+        if DEBUG:
+            warning.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +36,7 @@ SECRET_KEY = '#byf0&ja&ihor)bvfr50scwb_*lce!x_9)-zq+kn#dw_4ui@8i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aaronmilloro.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -137,6 +148,6 @@ INTERNAL_IPS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 
